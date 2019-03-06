4747670892e5958a0bafa7796144c88ea15b96bc
Javier Luraschi <javierluraschi@hotmail.com>
2019-03-05 22:08:31 -0800

    fix connection app

--- R/ui_connection.R
+++ R/ui_connection.R
@@ -1,3 +1,5 @@
+#' @keywords internal
+#' @export
 pinboard_connection_create <- function() {
   shinyAppDir <- get("shinyAppDir", envir = asNamespace("shiny"))
   shinyAppDir(system.file("rstudio/shinycon", package = "pinboard"))
