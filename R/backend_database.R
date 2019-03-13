dcdda31af46bd1faab9edb561232fe2752255e0c
Javier Luraschi <javierluraschi@hotmail.com>
2019-03-13 16:00:43 -0700

    sketch database board

---
+++ R/backend_database.R
@@ -0,0 +1,21 @@
+board_initialize.database <- function(...) {
+  args <- list(...)
+
+  args
+}
+
+pin_create.database <- function(board, dataset, name, description) {
+
+}
+
+pin_find.database <- function(board, name) {
+
+}
+
+pin_retrieve.database <- function(board, name) {
+
+}
+
+pin_remove.database <- function(board, name) {
+
+}
