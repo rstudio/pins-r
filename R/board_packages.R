0d3ac8c356dd69f02c20aee88da6d7e908958690
Javier Luraschi <javierluraschi@hotmail.com>
2019-03-20 17:52:35 -0700

    also search package names

--- R/board_packages.R
+++ R/board_packages.R
@@ -9,12 +9,13 @@ pin_find.packages <- function(board, text) {
     )
   }
 
+  find_packages <- grepl(text, crandatasets$package)
   find_names <- grepl(text, crandatasets$dataset)
   find_description <- grepl(text, crandatasets$description)
-  package_pins <- crandatasets[find_names | find_description,]
+  package_pins <- crandatasets[find_packages | find_names | find_description,]
 
   data.frame(
     name = package_pins$dataset,
-    description = package_pins$description
+    description = paste(gsub(" ?\\.$", "", package_pins$description), "from", package_pins$package, "package.")
   )
 }
