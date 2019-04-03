93b2f59c6e090a830e3ab1858e9b524babba0445
Javier Luraschi <javierluraschi@hotmail.com>
2019-04-03 13:39:14 -0700

    bind search textbox to shiny and back to results vue model

--- inst/rstudio/shinyaddin/app.R
+++ inst/rstudio/shinyaddin/app.R
@@ -4,7 +4,10 @@ pins_addin_source_choices <- function() {
 }
 
 pins_addin_server <- function(input, output, session) {
-
+  observe({
+    search <- input$search
+    session$sendCustomMessage("search-results", search)
+  })
 }
 
 shinyApp(htmlTemplate("www/index.html"), pins_addin_server)
