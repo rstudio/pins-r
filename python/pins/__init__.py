3ccd2f9532aea456d306a37f6dd113421bfecfbd
Javier Luraschi <javierluraschi@hotmail.com>
2019-04-12 14:53:50 -0700

    [Python] add empty pins pacakge

---
+++ python/pins/__init__.py
@@ -0,0 +1,10 @@
+"""
+The ``pins`` module provides an API for tracking, discovering and sharing datasets.
+"""
+
+def find_pin():
+    """
+    Find pins in the active board.
+    """
+    return []
+ 
---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pins',
+      version='0.1.0',
+      description='Track, Discover and Share Datasets',
+      url='https://github.com/rstudio/pins',
+      author='Javier Luraschi',
+      author_email='javier@rstudio.com',
+      license='Apache License 2.0',
+      packages=['pins'],
+      zip_safe=False)
