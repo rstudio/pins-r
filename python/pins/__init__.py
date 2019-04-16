34fe609fe83727ed7fd96dd87963471c278c72c7
Javier Luraschi <javierluraschi@hotmail.com>
2019-04-16 13:42:21 -0700

    [Python] naive initialization of embedded r

--- python/pins/__init__.py
+++ python/pins/__init__.py
@@ -4,6 +4,39 @@ The ``pins`` module provides an API for tracking, discovering and sharing datase
 
 import os
 import yaml
+from _pins_cffi import ffi
+import subprocess
+import platform
+
+def get_rhome():
+    r_home = os.environ.get("R_HOME")
+    if r_home:
+        return r_home
+    tmp = subprocess.check_output(("R", "RHOME"), universal_newlines=True)
+    r_home = tmp.split(os.linesep)
+    if r_home[0].startswith("WARNING"):
+        r_home = r_home[1]
+    else:
+        r_home = r_home[0].strip()
+    return r_home
+
+def get_rlib():
+    r_home = get_rhome()
+    system = platform.system()
+    if system == 'Linux':
+        lib_path = os.path.join(r_home, 'lib', 'libR.so')
+    elif system == 'Darwin':
+        lib_path = os.path.join(r_home, 'lib', 'libR.dylib')
+    else:
+        raise ValueError("System '%s' is unsupported.")
+    return lib_path
+
+def open_rlib():
+    return ffi.dlopen(get_rlib())
+
+def start():
+    rlib = open_rlib()
+    status = rlib.Rf_initialize_R(ffi.cast('int', 0), [])
 
 def find_pin():
     """
