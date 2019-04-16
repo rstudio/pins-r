b85d5ee1a53ffbf14b0c5074cab8ee9a43769314
Javier Luraschi <javierluraschi@hotmail.com>
2019-04-16 13:15:58 -0700

    [Python] add rembedded header

--- python/pins/pins_build.py
+++ python/pins/pins_build.py
@@ -1,7 +1,13 @@
 from cffi import FFI
 ffibuilder = FFI()
 
+# include/Rembedded.h
 ffibuilder.cdef("""
+int Rf_initialize_R(int ac, char **av);
+void R_RunExitFinalizers(void);
+void Rf_KillAllDevices(void);
+void R_CleanTempDir(void);
+void Rf_endEmbeddedR(int fatal);
 """)
 
 ffibuilder.set_source("_pins_cffi", None)
