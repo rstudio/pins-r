5ca3812614fdf9daba503690faed09df2bf9c062
Javier Luraschi <javierluraschi@hotmail.com>
2019-04-29 19:09:13 -0700

    support disabling tibble printing with pins.tibble option set to false

--- R/format.R
+++ R/format.R
@@ -1,5 +1,5 @@
 maybe_tibble <- function(data) {
-  if ("tibble" %in% installed.packages()) {
+  if ("tibble" %in% installed.packages() && !identical(getOption("pins.tibble"), FALSE)) {
     as_tibble <- get("as_tibble", envir = asNamespace("tibble"))
     as_tibble(data)
   }
