22715fb8942e62d30bedc7a58fc95d522884b4d5
Javier Luraschi <javierluraschi@hotmail.com>
2019-05-02 17:26:45 -0700

    search and retrieve kaggle datasets

--- R/board_kaggle.R
+++ R/board_kaggle.R
@@ -2,8 +2,37 @@ kaggle_dependencies <- function() {
 
 }
 
-board_initialize.kaggle <- function(...) {
+kaggle_auth_paths <- function() {
+  normalizePath(
+    "~/.kaggle/kaggle.json",
+    mustWork = FALSE
+  )
+}
 
+kaggle_authenticated <- function() {
+  any(file.exists(kaggle_auth_paths()))
+}
+
+kaggle_auth <- function() {
+  kaggle_keys <- jsonlite::read_json(kaggle_auth_paths())
+
+  httr::authenticate(
+    kaggle_keys$username,
+    kaggle_keys$key
+  )
+}
+
+board_initialize.kaggle <- function(board, token = NULL, overwrite = FALSE, ...) {
+  if (!is.null(token)) {
+    exists <- file.exists(kaggle_auth_paths())
+    if (exists && !overwrite) stop("File already exists, use 'overwrite = TRUE' to overwrite.")
+
+    if (!exists || identical(overwrite, TRUE)) {
+      token_path <- dirname(kaggle_auth_paths())
+      if (!dir.exists(token_path)) dir.create(token_path, recursive = TRUE)
+      file.copy(token, kaggle_auth_paths(), overwrite = TRUE)
+    }
+  }
 }
 
 pin_create.kaggle <- function(board, x, name, description, type, metadata) {
@@ -11,11 +40,38 @@ pin_create.kaggle <- function(board, x, name, description, type, metadata) {
 }
 
 pin_find.kaggle <- function(board, text) {
-  data.frame(name = c(), description = c(), type = c(), metadata = c())
+  if (!kaggle_authenticated())
+    stop("Missing Kaggle token file under. Configure running: register_board(\"kaggle\", )")
+
+  url <- paste0("https://www.kaggle.com/api/v1/datasets/list?search=", text)
+  results <- httr::content(httr::GET(url, config = kaggle_auth()))
+
+  results <- as.data.frame(do.call("rbind", results))
+
+  data.frame(
+    name = as.character(results$ref),
+    description = as.character(results$title),
+    type = "table",
+    metadata = "{}"
+  )
 }
 
 pin_retrieve.kaggle <- function(board, name) {
+  local_path <- file.path(pins:::pins_local_path("kaggle"), name)
 
+  if (dir.exists(local_path)) {
+    return(local_path)
+  }
+
+  url <- paste0("https://www.kaggle.com/api/v1/datasets/download/", name)
+  temp_zip <- tempfile(fileext = ".zip")
+
+  httr::content(httr::GET(url, config = auth, httr::write_disk(temp_zip)))
+
+  dir.create(local_path, recursive = TRUE)
+  unzip(temp_zip, exdir = local_path)
+
+  dir(local_path, full.names = TRUE)
 }
 
 pin_remove.kaggle <- function(board, name) {
@@ -23,10 +79,18 @@ pin_remove.kaggle <- function(board, name) {
 }
 
 board_info.kaggle <- function(board) {
-  list(
-    install_html = paste(
-      "Download token from:",
-      "<a href=\"https://www.kaggle.com/me/account\">kaggle.com/me/account</a>"
+  if (kaggle_authenticated()) {
+    install_html <- ""
+  }
+  else {
+    install_html <- paste(
+      "To search Kaggle datasets, <b>Create New API Token</b> from",
+      "<a href=\"https://www.kaggle.com/me/account\">kaggle.com/me/account</a>",
+      ", download and run register_board(\"<path-to-kaggle.json>\")"
     )
+  }
+
+  list(
+    install_html = install_html
   )
 }
