c6902e3c74d516c0df73968b6468b912328a512f
Javier Luraschi <javierluraschi@hotmail.com>
2019-05-06 21:48:25 -0700

    properly return rs pins

--- R/board_rstudio.R
+++ R/board_rstudio.R
@@ -5,36 +5,43 @@ rstudio_dependencies <- function() {
     deploy_app = get("deployApp", envir = asNamespace("rsconnect")),
     resolve_account = get("resolveAccount", envir = asNamespace("rsconnect")),
     account_info = get("accountInfo", envir = asNamespace("rsconnect")),
-    client_for_account = get("clientForAccount", envir = asNamespace("rsconnect"))
+    server_info = get("serverInfo", envir = asNamespace("rsconnect")),
+    client_for_account = get("clientForAccount", envir = asNamespace("rsconnect")),
+    accounts = get("accounts", envir = asNamespace("rsconnect")),
+    parse_http_url = get("parseHttpUrl", envir = asNamespace("rsconnect")),
+    get = get("GET", envir = asNamespace("rsconnect"))
   )
 }
 
-rstudio_api_get <- function(board, url) {
-  api_key <- rstudio_get_key(board)
+rstudio_account_info <- function(board) {
+  deps <- rstudio_dependencies()
 
-  httr::content(httr::GET(
-    url,
-    httr::add_headers(Authorization = paste("Key", api_key)),
-    httr::timeout(as.integer(Sys.getenv("RSTUDIO_CONNECT_API_TIMEOUT", 3)))
-  ))
+  account_name <- deps$resolve_account(account = board$account,
+                                       server = board$server)
+
+  deps$account_info(account_name)
 }
 
-rstudio_api_post <- function(board, url, object) {
-  api_key <- rstudio_get_key(board)
+rstudio_api_get <- function(board, path, root = FALSE) {
+  deps <- rstudio_dependencies()
 
-  httr::content(httr::POST(
-    url,
-    httr::add_headers(Authorization = paste("Key", api_key)),
-    httr::timeout(as.integer(Sys.getenv("RSTUDIO_CONNECT_API_TIMEOUT", 3))),
-    body = object
-  ))
+  server_info <- deps$server_info(board$server)
+  service <- deps$parse_http_url(server_info$url)
+  account_info <- rstudio_account_info(board)
+
+  if (root) service$path = gsub("/__api__", "", service$path)
+
+  deps$get(service, authInfo = account_info, path = path)
 }
 
 board_initialize.rstudio <- function(board, ...) {
   args <- list(...)
+  deps <- rstudio_dependencies()
 
   board$server <- args$server
   board$account <- args$account
+
+  if (is.null(args$server)) board$server <- deps$accounts()$server[1]
 
   board
 }
@@ -42,7 +49,9 @@ board_initialize.rstudio <- function(board, ...) {
 pin_create.rstudio <- function(board, x, name, description, type, metadata) {
   deps <- rstudio_dependencies()
 
-  csv_file <- tempfile(fileext = ".csv")
+  temp_dir <- tempfile()
+  dir.create(temp_dir)
+  csv_file <- file.path(temp_dir, "data.csv")
   write.csv(x, csv_file, row.names = FALSE)
 
   app <- deps$deploy_app(dirname(csv_file),
@@ -51,8 +60,7 @@ pin_create.rstudio <- function(board, x, name, description, type, metadata) {
                          appName = name,
                          server = board$server,
                          account = board$account,
-                         contentCategory = "data"
-                         )
+                         contentCategory = "data")
 
   unlink(csv_file)
 
@@ -63,8 +71,7 @@ pin_find.rstudio <- function(board, text, ...) {
   deps <- rstudio_dependencies()
   extended <- identical(list(...)$extended, TRUE)
 
-  account_name <- deps$resolve_account(account = NULL, server = board$server)
-  account_info <-deps$account_info(account_name)
+  account_info <- rstudio_account_info(board)
   client <- deps$client_for_account(account_info)
 
   results <- client$listApplications(accountId = account_info$accountId)
@@ -84,7 +91,7 @@ pin_find.rstudio <- function(board, text, ...) {
 }
 
 pin_retrieve.rstudio <- function(board, name, details) {
-  rstudio_api_get(board, as.character(details$url))
+  rstudio_api_get(board, paste0(gsub(".*/content", "/content", details$url), "data.csv"), root = TRUE)
 }
 
 pin_remove.rstudio <- function(board, name) {
