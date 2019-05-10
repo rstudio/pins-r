b9dfb0c887e09427ac97ba7737c5b45dd75764e8
Javier Luraschi <javierluraschi@hotmail.com>
2019-05-09 23:22:48 -0700

    reexport magrittr pipe

---
+++ R/reexports.R
@@ -0,0 +1,11 @@
+#' Pipe operator
+#'
+#' See \code{\link[dplyr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @usage lhs \%>\% rhs
+NULL
