9a286c6412a850b620507aa144e68fbb6495380f
Javier Luraschi <javierluraschi@hotmail.com>
2019-05-24 10:22:49 -0700

    add basic test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(pins)
+
+test_check("pins")
---
+++ tests/testthat/test-pin.R
@@ -0,0 +1,11 @@
+context("pin")
+
+test_that("can pin() file", {
+  file_path <- dir(getwd(), recursive = TRUE, pattern = "hello.txt", full.names = TRUE)
+
+  cached_path <- pin(file_path, "afile")
+
+  testthat::expect_true(is.character(cached_path))
+
+  testthat::expect_equal(readLines(cached_path), "hello world")
+})
