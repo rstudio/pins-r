3bfafd9d30182b5027a9f3d3a5d88bcaa6e1eb2b
Javier Luraschi <javierluraschi@hotmail.com>
2019-06-07 16:29:37 -0700

    a few more local board tests

--- tests/testthat/test-pin.R
+++ tests/testthat/test-pin.R
@@ -1,21 +1,45 @@
 context("pin")
 
+text_file <- dir(getwd(), recursive = TRUE, pattern = "hello.txt", full.names = TRUE)
+
 test_that("can pin() file", {
-  file_path <- dir(getwd(), recursive = TRUE, pattern = "hello.txt", full.names = TRUE)
+  cached_path <- pin(text_file, "afile")
 
-  cached_path <- pin(file_path, "afile")
+  expect_true(is.character(cached_path))
 
-  testthat::expect_true(is.character(cached_path))
-
-  testthat::expect_equal(readLines(cached_path), "hello world")
+  expect_equal(readLines(cached_path), "hello world")
 })
 
 test_that("can pin() file with auto-generated name", {
-  file_path <- dir(getwd(), recursive = TRUE, pattern = "hello.txt", full.names = TRUE)
+  cached_path <- pin(text_file)
 
-  cached_path <- pin(file_path)
+  expect_true(is.character(cached_path))
 
-  testthat::expect_true(is.character(cached_path))
+  expect_equal(readLines(cached_path), "hello world")
+})
 
-  testthat::expect_equal(readLines(cached_path), "hello world")
+test_that("can pin_get() a pin", {
+  pin(text_file, "afile")
+
+  cached_path <- pin_get("afile")
+
+  expect_true(is.character(cached_path))
+
+  expect_equal(readLines(cached_path), "hello world")
+})
+
+test_that("can pin_find() a in any board", {
+  pin(text_file, "afile")
+
+  results <- pin_find("afile")
+
+  expect_true("afile" %in% results$name)
+})
+
+test_that("can pin_find() in local board", {
+  pin(text_file, "afile")
+
+  results <- pin_find("afile", board = "local")
+
+  expect_true("afile" %in% results$name)
 })
