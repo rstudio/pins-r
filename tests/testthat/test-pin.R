1e1e9ff7a7c33f48b753ef2f623e7258d2a4a0b3
Javier Luraschi <javierluraschi@hotmail.com>
2019-06-10 11:25:03 -0700

    add test to pin objects

--- tests/testthat/test-pin.R
+++ tests/testthat/test-pin.R
@@ -43,3 +43,13 @@ test_that("can pin_find() in local board", {
 
   expect_true("afile" %in% results$name)
 })
+
+test_that("can pin() object", {
+  an_object <- tibble::tibble(numbers = c(1, 2, 3),
+                              text = c("a", "b", "c"))
+
+  roundtrip <- pin(an_object, "anobject")
+
+  expect_equal(an_object, roundtrip)
+  expect_equal(an_object, pin_get("anobject"))
+})
