7267950108f2136861e77cb542bb8c63de31d601
Javier Luraschi <javierluraschi@hotmail.com>
2019-06-11 11:23:26 -0700

    fix test warning

--- tests/testthat/test-pin-files.R
+++ tests/testthat/test-pin-files.R
@@ -1,7 +1,7 @@
 context("pin local")
 
 test_that("can pin() remote URL", {
-  rstudio_page <- readLines(pin("https://www.google.com/"))
+  rstudio_page <- readLines(pin("https://www.google.com/"), warn = FALSE)
 
   expect_gt(length(rstudio_page), 0)
 })
