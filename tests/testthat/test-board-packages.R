d803bac55dcc31cfb302a807b1eead9701a44208
Javier Luraschi <javierluraschi@hotmail.com>
2019-06-12 12:10:09 -0700

    test pin_get() from packages

--- tests/testthat/test-board-packages.R
+++ tests/testthat/test-board-packages.R
@@ -3,11 +3,18 @@ context("board packages")
 test_that("can pin_find() packages with empty search", {
   results <- pin_find(board = "packages")
 
-  testthat::expect_gt(nrow(results), 0)
+  expect_gt(nrow(results), 0)
 })
 
 test_that("can pin_find() packages with search term", {
   results <- pin_find("seattle", board = "packages")
 
-  testthat::expect_gt(nrow(results), 0)
+  expect_gt(nrow(results), 0)
+})
+
+test_that("can pin_get() an specific resource", {
+  expect_gt(
+    nrow(pin_get("hpiR/seattle_sales", board = "packages")),
+    10^4
+  )
 })
