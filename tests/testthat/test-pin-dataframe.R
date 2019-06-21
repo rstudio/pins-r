bac441b80141f88d44233d211707fe00df0d014a
Javier Luraschi <javierluraschi@hotmail.com>
2019-06-21 10:34:38 -0700

    pin auto generated names

--- tests/testthat/test-pin-dataframe.R
+++ tests/testthat/test-pin-dataframe.R
@@ -6,3 +6,16 @@ test_that("can pin() data frame", {
   expect_equal(as.data.frame(roundtrip), iris)
   expect_equal(as.data.frame(pin_get("iris")), iris)
 })
+
+test_that("can sanitize data frame names", {
+  name <- "___sdf ds32___42342     dsf dsf dsfds____"
+  expect_equal(
+    pin_dataframe_sanitize(name),
+    "sdf-ds32-42342-dsf-dsf-dsfds"
+  )
+
+  expect_equal(
+    pin_dataframe_sanitize("iris"),
+    "iris-pin"
+  )
+})
