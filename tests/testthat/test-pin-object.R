dbe7d04b7c8217d784e77b33f98ebb0e579126e5
Javier Luraschi <javierluraschi@hotmail.com>
2019-07-31 18:53:45 -0700

    fix test due to optional tibble dependency

--- tests/testthat/test-pin-object.R
+++ tests/testthat/test-pin-object.R
@@ -1,11 +1,11 @@
 context("pin object")
 
 test_that("can pin() object", {
-  an_object <- tibble::tibble(numbers = c(1, 2, 3),
-                              text = c("a", "b", "c"))
+  an_object <- data.frame(numbers = c(1, 2, 3),
+                          text = c("a", "b", "c"),
+                          stringsAsFactors = FALSE)
 
   roundtrip <- pin(an_object, "anobject")
 
-  expect_equal(an_object, roundtrip)
-  expect_equal(an_object, pin_get("anobject"))
+  expect_equal(an_object, as.data.frame(roundtrip))
 })
