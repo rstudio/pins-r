37fcf729c731d92ba84ef522e705ca9a0ade3cc5
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-09 13:55:23 -0700

    re-enable rsc pin_remove() test

--- tests/testthat/test-board-rsconnect.R
+++ tests/testthat/test-board-rsconnect.R
@@ -1,7 +1,7 @@
 context("board rsc")
 
 if (test_board_is_registered("rsconnect")) {
-  board_test("rsconnect", exclude = "remove")
+  board_test("rsconnect")
 } else {
   test_that("can't register rsconnect board", {
     skip("failed to register rsconnect board")
