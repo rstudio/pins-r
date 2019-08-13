22e94770ca4df8c627249a49b849eaba61e77754
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-13 10:22:55 -0700

    add board extension api to site

--- R/board_extensions.R
+++ R/board_extensions.R
@@ -1,20 +1,22 @@
 #' Custom Boards
 #'
 #' Family of functions meant to be used to implement custom boards extensions,
-#' not to be used by end users.
+#' not to be used by users.
 #'
-#' @param x A local file path or an object. Boards must support storing both.
+#' @param board The board to extended, retrieved with \code{board_get()}.
+#' @param name The name of the pin.
+#' @param path The path to store.
+#' @param text The text patteren to find a pin.
+#' @param ... Additional parameteres.
 #'
 #' @rdname custom-boards
 #' @export
-#' @keywords internal
-board_pin_create <- function(board, path, name, description, type, metadata) {
+board_pin_create <- function(board, path, name, ...) {
   UseMethod("board_pin_create")
 }
 
 #' @export
 #' @rdname custom-boards
-#' @keywords internal
 board_initialize <- function(board, ...) {
   UseMethod("board_initialize")
 }
@@ -23,27 +25,28 @@ board_initialize.default <- function(board, ...) stop("Board '", board$name, "' 
 
 #' @export
 #' @rdname custom-boards
-#' @keywords internal
-board_pin_get <- function(board, name, details) {
+board_pin_get <- function(board, name, details, ...) {
   UseMethod("board_pin_get")
 }
 
 #' @export
 #' @rdname custom-boards
-#' @keywords internal
-board_pin_remove <- function(board, name) {
+board_pin_remove <- function(board, name, ...) {
   UseMethod("board_pin_remove")
 }
 
 #' @export
 #' @rdname custom-boards
-#' @keywords internal
 board_pin_find <- function(board, text, ...) {
   UseMethod("board_pin_find")
 }
 
+#' Custom Boards Utilities
+#'
+#' A set of utilities used when implementing custom boards.
+#'
 #' @export
-#' @rdname custom-boards
+#' @rdname custom-boards-utils
 #' @keywords internal
 board_local_storage <- function(component) {
   paths <- list(
@@ -64,8 +67,7 @@ board_local_storage <- function(component) {
 
 #' @export
 #' @rdname custom-boards
-#' @keywords internal
-board_load <- function(board) {
+board_load <- function(board, ...) {
   UseMethod("board_load")
 }
 
@@ -73,7 +75,7 @@ board_load.default <- function(board) board
 
 #' @export
 #' @rdname custom-boards
-board_persist <- function(board) {
+board_persist <- function(board, ...) {
   UseMethod("board_persist")
 }
 
@@ -81,7 +83,6 @@ board_persist.default <- function(board) structure(list(board = board$board, nam
 
 #' @export
 #' @rdname custom-boards
-#' @keywords internal
 board_pin_store <- function(board, path, name, description, type, metadata, ...) {
   if (is.null(name)) name <- gsub("[^a-zA-Z0-9]+", "_", tools::file_path_sans_ext(basename(path)))
 
@@ -113,8 +114,7 @@ board_pin_store <- function(board, path, name, description, type, metadata, ...)
 
 #' @export
 #' @rdname custom-boards
-#' @keywords internal
-board_browse <- function(board) {
+board_browse <- function(board, ...) {
   UseMethod("board_browse")
 }
 
