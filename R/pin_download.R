68f5e932cc459cad85b55ee84d232a462118ede7
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-14 12:01:16 -0700

    some kaggle files have no zip content type like austin-weather

--- R/pin_download.R
+++ R/pin_download.R
@@ -83,6 +83,10 @@ pin_download <- function(path, name, component, ...) {
         write_spec <- httr::write_disk(destination_path, overwrite = TRUE)
         result <- httr::GET(path, write_spec, headers, config, http_utils_progress())
         is_zip <- identical(result$headers$`content-type`, "application/zip")
+        if (!is_zip && identical(result$headers$`content-type`, "application/octet-stream")) {
+          is_zip <- file.size(destination_path) > 4 &&
+            identical(readBin(destination_path, raw(), 4), as.raw(c(0x50, 0x4b, 0x03, 0x04)))
+        }
 
         if (httr::http_error(result)) {
           error <- paste0(status, " Failed to download remote file: ", path)
