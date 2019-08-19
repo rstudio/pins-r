5228bf5753394ce07546193a5822baebfa1ee548
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-19 13:12:51 -0700

    add board_register_kaggle()

--- R/board_registration.R
+++ R/board_registration.R
@@ -7,12 +7,12 @@
 #' @param name Optional name for this board, defaults to 'github'.
 #' @param branch The branch to use when commiting pins.
 #' @param token Token to use when \code{GITHUB_PAT} is not specified.
-#' @param path The subdireectory in the repo where the pins will be stored.
+#' @param path The subdirectory in the repo where the pins will be stored.
 #'
 #' @seealso board_register
 #'
 #' @examples
-#' board_register("github", repo = "datatxtorg/datatxt-site")
+#' board_register_github("github", repo = "datatxtorg/datatxt-site")
 #'
 #' @export
 board_register_github <- function(repo,
@@ -21,4 +21,25 @@ board_register_github <- function(repo,
                                   token = NULL,
                                   path = "") {
   board_register("github", name = name, repo = repo, branch = branch, token = token, path = path)
+}
+
+#' Register Kaggle Board
+#'
+#' Wrapper with explicit parameters over \code{board_register()} to
+#' register Kaggle as a board.
+#'
+#' @param token The Kaggle token as a path to the \code{kaggle.json} file.
+#' @param name Optional name for this board, defaults to 'kaggle'.
+#'
+#' @seealso board_register
+#'
+#' @examples
+#' \dontrun{
+#' board_register_kaggle("kaggle", token = "path/to/kaggle.json")
+#' }
+#'
+#' @export
+board_register_kaggle <- function(token,
+                                  name = "kaggle") {
+  board_register("kaggle", name = name, token = repo)
 }
