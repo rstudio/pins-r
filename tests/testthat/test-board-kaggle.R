8016480cd48a2dfd46cc24f2c93eb4002facc0e4
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-24 14:57:56 -0700

    add readonly kaggle tests to travis

--- tests/testthat/test-board-kaggle.R
+++ tests/testthat/test-board-kaggle.R
@@ -1,9 +1,36 @@
 context("board kaggle")
 
+test_github_repo <- Sys.getenv("TEST_KAGGLE_API", "")
+if (nchar(test_github_repo) > 0) {
+  if ("kaggle" %in% board_list()) board_deregister("kaggle")
+}
+
 if (test_board_is_registered("kaggle")) {
   board_test("kaggle", exclude = "remove")
 } else {
   test_that("can't register kaggle board", {
     skip("failed to register kaggle board")
+  })
+}
+
+if (nchar(test_github_repo) > 0) {
+  test_that("can board_register() kaggle board", {
+    base64enc::base64decode(test_github_repo) %>%
+      rawToChar() %>%
+      writeLines("kaggle.json")
+
+    board_register("kaggle", token = "kaggle.json")
+
+    expect_true("kaggle" %in% board_list())
+  })
+
+  test_that("can pin_find() 'seattle' in kaggle board", {
+    searches <- pin_find("game of thrones", board = "kaggle")
+    expect_gt(nrow(searches), 0)
+  })
+
+  test_that("can pin_get() 'got' in kaggle board", {
+    dataset <- pin_get("gunnvant/game-of-thrones-srt", board = "kaggle")
+    expect_gt(length(dataset), 0)
   })
 }
