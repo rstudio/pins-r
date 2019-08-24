1edb9693d61cfbeed1ae513d0abb3b9ac5956492
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-24 10:26:29 -0700

    add github board tests to travis

--- tests/testthat/test-board-github.R
+++ tests/testthat/test-board-github.R
@@ -1,5 +1,10 @@
 context("board github")
 
+test_github_repo <- Sys.getenv("TEST_GITHUB_REPO", "")
+if (nchar(test_github_repo) > 0) {
+  board_register("github", repo = test_github_repo)
+}
+
 if (test_board_is_registered("github")) {
   board_test("github")
 } else {
