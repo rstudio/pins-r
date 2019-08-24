36c11a32ff1892a8bc868c732a692d107e82ec19
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-24 11:20:02 -0700

    split travis tests into github branches

--- tests/testthat/test-board-github.R
+++ tests/testthat/test-board-github.R
@@ -1,8 +1,10 @@
 context("board github")
 
 test_github_repo <- Sys.getenv("TEST_GITHUB_REPO", "")
+test_github_branch <- Sys.getenv("TEST_GITHUB_BRANCH", "")
 if (nchar(test_github_repo) > 0) {
-  board_register("github", repo = test_github_repo)
+  if (nchar(test_github_branch) == 0) test_github_branch <- NULL
+  board_register("github", repo = test_github_repo, branch = test_github_branch)
 }
 
 if (test_board_is_registered("github")) {
