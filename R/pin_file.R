a3664fef6ca60b72a629cb8efa0bc0d05f2972c6
Javier Luraschi <javierluraschi@hotmail.com>
2019-09-06 15:47:58 -0700

    properly store file ext in metadata

--- R/pin_file.R
+++ R/pin_file.R
@@ -16,5 +16,6 @@ pin_file_cache_max_age <- function(cache_control) {
 #' @keywords internal
 #' @export
 pin.character <- function(x, name = NULL, description = NULL, board = NULL, ...) {
-  board_pin_store(board, x, name, description, "files", list(), ...)
+  extension <- if (length(x) > 1) "zip" else tools::file_ext(x)
+  board_pin_store(board, x, name, description, "files", list(extension = extension), ...)
 }
