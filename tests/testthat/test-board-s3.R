3fd0172377563ed3ee93d79c207a5c657c387257
Javier Luraschi <javierluraschi@hotmail.com>
2019-10-28 21:54:08 -0700

    troubleshoot s3 travis tests

--- tests/testthat/test-board-s3.R
+++ tests/testthat/test-board-s3.R
@@ -4,6 +4,10 @@ test_s3_bucket <- Sys.getenv("TEST_AWS_BUCKET", "")
 test_s3_key <- Sys.getenv("TEST_AWS_KEY", "")
 test_s3_secret <- Sys.getenv("TEST_AWS_SECRET", "")
 
+test_that("can pin large resources in github releases", {
+  expect_equal(format(Sys.time(), "%a, %d %b %Y %X %z"), "")
+})
+
 if (nchar(test_s3_bucket) > 0) {
   if ("s3" %in% board_list())
     board_deregister("s3")
