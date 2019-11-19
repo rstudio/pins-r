c96693a391ec3a54cf7986a2cfc8c57539f957b9
Javier Luraschi <javierluraschi@hotmail.com>
2019-11-18 17:11:02 -0800

    avoid final line warning when loading pin manifest

--- R/board_manifest.R
+++ R/board_manifest.R
@@ -1,9 +1,9 @@
 board_manifest_get <- function(path) {
-  yaml::read_yaml(path, eval.expr = FALSE)
+  suppressWarnings(yaml::read_yaml(path, eval.expr = FALSE))
 }
 
 board_manifest_load <- function(manifest) {
-  yaml::yaml.load(manifest, eval.expr = FALSE)
+  suppressWarnings(yaml::yaml.load(manifest, eval.expr = FALSE))
 }
 
 board_manifest_create <- function(index, file) {
