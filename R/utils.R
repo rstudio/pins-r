61ae7aefbac21cd6e138a3f637f84afadce9b05e
Javier Luraschi <javierluraschi@hotmail.com>
2019-11-19 12:04:45 -0800

    fix issue where download progress would show randomly

--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,8 @@ get_function <- function(name, package) {
 }
 
 pins_show_progress <- function(size = 0) {
+  if (is.character(size)) size <- as.integer(size)
+
   large_file <- getOption("pins.progress.size", 10^7)
   identical(getOption("pins.progress", size > large_file), TRUE)
 }
