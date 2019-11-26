3c86a35af1c1a809359573ff20c82635e8a17710
Javier Luraschi <javierluraschi@hotmail.com>
2019-11-26 04:02:15 -0800

    also clean png images

--- tools/release-cran.R
+++ tools/release-cran.R
@@ -11,6 +11,8 @@ for (file in image_files) {
 
   target_file <- paste(tools::file_path_sans_ext(file), ".jpg")
   system2("convert", c(file, "-resize", "20%", target_file))
+
+  unlink(file)
 }
 
 # R CMD build --resave-data pins
