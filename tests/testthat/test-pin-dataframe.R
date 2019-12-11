d82ed228e1feae1a7308d087331313fb7e04b5bd
Javier Luraschi <javierluraschi@hotmail.com>
2019-12-11 12:15:51 -0800

    pin dataframes csv with supported columns and warn to fix #142

--- tests/testthat/test-pin-dataframe.R
+++ tests/testthat/test-pin-dataframe.R
@@ -14,3 +14,26 @@ test_that("can sanitize data frame names", {
     "sdf-ds32-42342-dsf-dsf-dsfds"
   )
 })
+
+test_that("can save data frame with different types", {
+  test_df <- data.frame(raw =  charToRaw("asdas"),
+                        posix = as.POSIXlt(Sys.time(), "EST"),
+                        date = as.Date(35981, origin = "1899-12-30"),
+                        integer = 1L,
+                        numeric = 1,
+                        booleaen = TRUE)
+
+  pins_safe_csv(test_df, "test_df.csv")
+
+  expect_true(file.exists("test_df.csv"))
+
+  unlink("test_df.csv")
+})
+
+test_that("can pin complex data frame", {
+  complex_df <- readRDS("files/complex-df.rds")
+
+  pin(complex_df, "complex-df")
+
+  succeed()
+})
