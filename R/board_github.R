857ed2be29c33fec88eea7b2cfb88878fbe1cf15
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-21 17:16:28 -0800

    fix typo introduced in previous commit

--- R/board_github.R
+++ R/board_github.R
@@ -362,7 +362,7 @@ github_branches_create <- function(board, new_branch, base_branch) {
 github_content_url <- function(board, branch = board$branch, ...) {
   args <- list(...)
 
-  url <- paste0(bost$host, "/repos/", board$repo, "/contents/", paste0(board$path, paste0(args, collapse = "")))
+  url <- paste0(board$host, "/repos/", board$repo, "/contents/", paste0(board$path, paste0(args, collapse = "")))
   if (!is.null(branch))
     url <- paste0(url, "?ref=", branch)
 
