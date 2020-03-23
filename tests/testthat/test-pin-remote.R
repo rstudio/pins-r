934da6c23211102152f319f1a533e2c2c74c22f5
Javier Luraschi <javierluraschi@hotmail.com>
2020-03-23 11:24:29 -0700

    add tests for signature cheecks

--- tests/testthat/test-pin-remote.R
+++ tests/testthat/test-pin-remote.R
@@ -24,3 +24,19 @@ test_that("can pin() remote CSV with URL and name", {
   expect_gt(nrow(retail_sales), 10)
 })
 
+test_that("can pin() remote CSV with signature", {
+  skip_on_cran()
+
+  retail_sales <- read.csv(pin("http://www.fhfa.gov/DataTools/Downloads/Documents/HPI/HPI_master.csv", signature = "264ea461b5f73a50e5935ab0ba2fb062fbef642d"))
+
+  expect_gt(nrow(retail_sales), 10)
+})
+
+test_that("fail to pin() remote CSV with bad signature", {
+  skip_on_cran()
+
+  expect_error({
+    pin("http://www.fhfa.gov/DataTools/Downloads/Documents/HPI/HPI_master.csv", signature = "aaaaa461b5f73a50e5935ab0ba2fb062fbef642d")
+  })
+})
+
