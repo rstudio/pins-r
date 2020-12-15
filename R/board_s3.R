12b62bb30706625397a9886e1a99d5c6ab245c24
Javier Luraschi <javierluraschi@hotmail.com>
2020-12-14 21:38:10 -0800

    draft aws s3 v4 signature

--- R/board_s3.R
+++ R/board_s3.R
@@ -1,3 +1,100 @@
+# See https://docs.amazonaws.cn/en_us/general/latest/gr/sigv4-signed-request-examples.html#sig-v4-examples-get-auth-header
+# httr::GET("https://ec2.amazonaws.com?Action=DescribeRegions&Version=2013-10-15", pins:::s3_headers_v4()) %>% httr::text_content()
+s3_headers_v4 <- function(method = "GET",
+                          service = "ec2",
+                          host = "ec2.amazonaws.com",
+                          region = "us-east-1",
+                          endpoint = "https://ec2.amazonaws.com",
+                          request_parameters = "Action=DescribeRegions&Version=2013-10-15") {
+  # Key derivation functions. See:
+  # http://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
+  sign <- function(key, msg) {
+    openssl::sha256(charToRaw(enc2utf8(msg)), key = key)
+  }
+
+  getSignatureKey <- function(key, dateStamp, regionName, serviceName) {
+    kDate <- sign(paste0("AWS4", key), dateStamp)
+    kRegion <- sign(kDate, regionName)
+    kService <- sign(kRegion, serviceName)
+    kSigning <- sign(kService, "aws4_request")
+    kSigning
+  }
+
+  # Read AWS access key from env. variables or configuration file. Best practice is NOT
+  # to embed credentials in code.
+  access_key <- Sys.getenv("AWS_ACCESS_KEY_ID")
+  secret_key <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
+
+  # Create a date for headers and the credential string
+  amzdate <- format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = "GMT")
+  datestamp <- format(Sys.time(), "%Y%m%d", tz = "GMT")
+
+  # ************* TASK 1: CREATE A CANONICAL REQUEST *************
+  # http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+
+  # Step 1 is to define the verb (GET, POST, etc.)--already done.
+
+  # Step 2: Create canonical URI--the part of the URI from domain to query
+  # string (use "/" if no path)
+  canonical_uri <- "/"
+
+  # Step 3: Create the canonical query string. In this example (a GET request),
+  # request parameters are in the query string. Query string values must
+  # be URL-encoded (space=%20). The parameters must be sorted by name.
+  # For this example, the query string is pre-formatted in the request_parameters variable.
+  canonical_querystring <- request_parameters
+
+  # Step 4: Create the canonical headers. Header names must be trimmed
+  # and lowercase, and sorted in code point order from low to high.
+  # Note that there is a trailing \n.
+  canonical_headers <- paste0(
+    "host:", host, "\n",
+    "x-amz-date:", amzdate, "\n",
+    "")
+
+  # Step 5: Create the list of signed headers. This lists the headers
+  signed_headers <- "host;x-amz-date"
+
+  # Step 6: Create payload hash. In this example, the payload (body of
+  # the request) contains the request parameters.
+  payload_hash <- digest::digest(enc2utf8(""), serialize = FALSE, algo = "sha256")
+
+  # Step 7: Combine elements to create canonical request
+  canonical_request <- paste0(method, "\n", canonical_uri, "\n", canonical_querystring, "\n", canonical_headers, "\n", signed_headers, "\n", payload_hash)
+
+  # ************* TASK 2: CREATE THE STRING TO SIGN*************
+  # Match the algorithm to the hashing algorithm you use, either SHA-1 or
+  # SHA-256 (recommended)
+  algorithm <- "AWS4-HMAC-SHA256"
+  credential_scope <- paste0(datestamp, "/", board$region, "/", service, "/", "aws4_request")
+  string_to_sign <- paste0(algorithm, "\n",  amzdate, "\n", credential_scope, "\n",  digest::digest(enc2utf8(canonical_request), serialize = FALSE, algo = "sha256"))
+
+  # ************* TASK 3: CALCULATE THE SIGNATURE *************
+  # Create the signing key using the function defined above.
+  signing_key <- getSignatureKey(secret_key, datestamp, region, service)
+
+  # Sign the string_to_sign using the signing_key
+  signature <- openssl::sha256(string_to_sign, key = signing_key) %>% as.character()
+
+  # ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
+  # Put the signature information in a header named Authorization.
+  authorization_header <- paste0(
+    algorithm, " ",
+    "Credential=", board$key, "/", credential_scope, ", ",
+    "SignedHeaders=", signed_headers, ", ",
+    "Signature=", signature)
+
+  headers <- httr::add_headers(
+    # "Host" = host,
+    # "Content-Type" = content_type,
+    "x-amz-date" = amzdate,
+    # "x-amz-content-sha256" = amz_content_sha256,
+    "Authorization" = authorization_header
+  )
+
+  headers
+}
+
 s3_headers <- function(board, verb, path, file) {
   date <- format(Sys.time(), "%a, %d %b %Y %H:%M:%S %z")
 
@@ -9,24 +106,29 @@ s3_headers <- function(board, verb, path, file) {
     bucket <- gsub("\\..*", "", path_nohttp)
   }
 
-  content <- paste(
-    verb,
-    "",
-    "application/octet-stream",
-    date,
-    file.path("", bucket, path),
-    sep = "\n"
-  )
+  if (!identical(board$region, NULL)) {
 
-  signature <- openssl::sha1(charToRaw(content), key = board$secret) %>%
-    base64enc::base64encode()
+  }
+  else {
+    content <- paste(
+      verb,
+      "",
+      "application/octet-stream",
+      date,
+      file.path("", bucket, path),
+      sep = "\n"
+    )
 
-  headers <- httr::add_headers(
-    Host = paste0(bucket, ".", board$host),
-    Date = date,
-    `Content-Type` = "application/octet-stream",
-    Authorization = paste0("AWS ", board$key, ":", signature)
-  )
+    signature <- openssl::sha1(charToRaw(content), key = board$secret) %>%
+      base64enc::base64encode()
+
+    headers <- httr::add_headers(
+      Host = paste0(bucket, ".", board$host),
+      Date = date,
+      `Content-Type` = "application/octet-stream",
+      Authorization = paste0("AWS ", board$key, ":", signature)
+    )
+  }
 
   headers
 }
