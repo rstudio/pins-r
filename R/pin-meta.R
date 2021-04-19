6345e9db1b2d810ae0bea3c4ac4d63953b8dc87c
Hadley Wickham <h.wickham@gmail.com>
2021-04-19 18:00:57 -0500

    Improve pins_meta printing

--- R/pin-meta.R
+++ R/pin-meta.R
@@ -1,6 +1,7 @@
 #' Retrieve metadata for a pin
 #'
 #' @inheritParams pin_read
+#' @returns A list.
 #' @export
 #' @examples
 #' b <- board_temp()
@@ -9,10 +10,16 @@
 #' # Get the pin
 #' b %>% pin_read("mtcars")
 #' # Get its metadata
-#' b %>% pin_meta("mtcars") %>% str()
+#' b %>% pin_meta("mtcars")
 #' # Get path to underlying data
 #' b %>% pin_download("mtcars")
 pin_meta <- function(board, name, version = NULL, hash = NULL, ...) {
   pin <- pin_retrieve(board, name, version = version, hash = hash, ...)
-  pin$meta
+  structure(pin$meta, class = "pins_meta")
+}
+
+#' @export
+print.pins_meta <- function(x, ...) {
+  utils::str(unclass(x))
+  invisible(x)
 }
