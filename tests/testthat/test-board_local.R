162c4a04e231a6ad8cf5169474def369e34b1f47
Hadley Wickham <h.wickham@gmail.com>
2021-04-22 12:25:52 -0500

    Use pin_list() instead of pin_find()

--- tests/testthat/test-board_local.R
+++ tests/testthat/test-board_local.R
@@ -9,15 +9,14 @@ test_that("has useful print method", {
 test_that(paste("can pin() file with auto-generated name in local board"), {
   b <- board_temp()
 
-  expect_equal(nrow(pin_find(board = b)), 0)
+  expect_equal(pin_list(b), character())
 
   hello <- test_path("files/hello.txt")
   path <- pin(hello, board = b)
   expect_true(file.exists(path))
   expect_equal(readLines(path), "hello world")
 
-  pins <- pin_find(, board = b)
-  expect_equal(pins$name, "hello")
+  expect_equal(pin_list(b), "hello")
 })
 
 test_that("can remove a local pin", {
