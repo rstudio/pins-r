bbca0298c4a5e81be13546b7508dd4b815136a0a
Hadley Wickham <h.wickham@gmail.com>
2021-04-22 13:20:59 -0500

    Tidy up RSC tests

--- tests/testthat/test-board_rsconnect.R
+++ tests/testthat/test-board_rsconnect.R
@@ -4,10 +4,10 @@ test_that("can round-trip a pin (v1)", {
   board <- board_rsconnect_test()
 
   df1 <- data.frame(x = 1:5)
-  pin_write(board, df1, "df1", type = "rds")
-  withr::defer(board_pin_remove(board, "df1"))
+  pin_write(board, df1, "test-df1", type = "rds")
+  withr::defer(pin_delete(board, "test-df1"))
 
-  df2 <- pin_read(board, "df1")
+  df2 <- pin_read(board, "test-df1")
   expect_equal(df1, df2)
 })
 
@@ -15,17 +15,17 @@ test_that("can round-trip a pin (v0)", {
   board <- board_rsconnect_test()
 
   df1 <- data.frame(x = 1:5)
-  pin(df1, "df1", board = board)
-  withr::defer(board_pin_remove(board, "df1"))
+  pin(df1, "test-df1", board = board)
+  withr::defer(pin_delete(board, "test-df1"))
 
-  df2 <- pin_get("df1", board = board)
+  df2 <- pin_get("test-df1", board = board)
   expect_equal(df1, df2)
 })
 
 test_that("can find/search pins", {
   board <- board_rsconnect_test()
-  board %>% pin_write(1:5, "xyzxyzxyzxyz-abc", desc = "defdefdef")
-  withr::defer(board_pin_remove(board, "xyzxyzxyzxyz-abc"))
+  board %>% pin_write(1:5, "test-xyzxyzxyzxyz", desc = "defdefdef")
+  withr::defer(pin_delete(board, "test-xyzxyzxyzxyz"))
 
   expect_equal(nrow(board_pin_find(board, "xyzxyzxyzxyz")), 1)
   expect_equal(nrow(board_pin_find(board, "abcabcabc")), 0)
@@ -45,9 +45,9 @@ test_that("can upload/download multiple files", {
 
   board <- board_rsconnect_test()
   suppressMessages(pin_upload(board, c(path1, path2), "test-multi-file"))
-  withr::defer(board_pin_remove(board, "test-multi-file"))
+  withr::defer(pin_delete(board, "test-multi-file"))
 
-  out <- pin_download(board, "test")
+  out <- pin_download(board, "test-multi-file")
   expect_equal(length(out), 2)
   expect_equal(readLines(out[[1]]), "a")
   expect_equal(readLines(out[[2]]), "b")
@@ -58,42 +58,42 @@ test_that("can upload/download multiple files", {
 
 test_that("versioned by default", {
   board <- board_rsconnect_test()
-  withr::defer(board_pin_remove(board, "df1"))
+  withr::defer(pin_delete(board, "test-df1"))
 
-  pin_write(board, data.frame(x = 1:3), "df1", type = "rds")
-  pin_write(board, data.frame(x = 1:4), "df1", type = "rds")
-  pin_write(board, data.frame(x = 1:5), "df1", type = "rds")
+  pin_write(board, data.frame(x = 1:3), "test-df1", type = "rds")
+  pin_write(board, data.frame(x = 1:4), "test-df1", type = "rds")
+  pin_write(board, data.frame(x = 1:5), "test-df1", type = "rds")
 
-  versions <- pin_versions(board, "df1")
+  versions <- pin_versions(board, "test-df1")
   expect_equal(nrow(versions), 3)
 
-  df2 <- pin_read(board, "df1", version = versions$version[[2]])
+  df2 <- pin_read(board, "test-df1", version = versions$version[[2]])
   expect_equal(df2, data.frame(x = 1:4))
 })
 
 test_that("if unversioned, deletes last one", {
   board <- board_rsconnect_test(versions = FALSE)
-  withr::defer(board_pin_remove(board, "df1"))
+  withr::defer(pin_delete(board, "test-df1"))
 
-  pin_write(board, data.frame(x = 1), "df1", type = "rds")
-  pin_write(board, data.frame(x = 2), "df1", type = "rds")
+  pin_write(board, data.frame(x = 1), "test-df1", type = "rds")
+  pin_write(board, data.frame(x = 2), "test-df1", type = "rds")
 
-  guid <- rsc_content_find(board, "df1")$guid
-  expect_equal(nrow(pin_versions(board, "df1")), 1)
+  guid <- rsc_content_find(board, "test-df1")$guid
+  expect_equal(nrow(pin_versions(board, "test-df1")), 1)
 
-  df2 <- pin_read(board, "df1")
+  df2 <- pin_read(board, "test-df1")
   expect_equal(df2$x, 2)
 })
 
 test_that("can't accidentally switch from versioned to unversioned", {
   board <- board_rsconnect_test()
-  withr::defer(board_pin_remove(board, "df1"))
+  withr::defer(pin_delete(board, "test-df1"))
 
   df1 <- data.frame(x = 1:3)
-  pin_write(board, df1, "df1", type = "rds")
-  pin_write(board, df1, "df1", type = "rds")
+  pin_write(board, df1, "test-df1", type = "rds")
+  pin_write(board, df1, "test-df1", type = "rds")
   expect_snapshot(error = TRUE,
-    pin_write(board, df1, "df1", type = "rds", versioned = FALSE)
+    pin_write(board, df1, "test-df1", type = "rds", versioned = FALSE)
   )
 })
 
@@ -102,11 +102,14 @@ test_that("can't accidentally switch from versioned to unversioned", {
 test_that("can find content by full/partial name", {
   board <- board_rsconnect_test()
 
-  json1 <- rsc_content_find(board, "sales-by-baths")
-  json2 <- rsc_content_find(board, "hadley/sales-by-baths")
+  pin_write(board, 1:3, "test-partial", type = "rds")
+  withr::defer(pin_delete(board, "test-partial"))
+
+  json1 <- rsc_content_find(board, "test-partial")
+  json2 <- rsc_content_find(board, "hadley/test-partial")
   expect_equal(json1$guid, json2$guid)
 
-  expect_snapshot(rsc_content_find(board, "susan/sales-by-baths"), error = TRUE)
+  expect_snapshot(rsc_content_find(board, "susan/test-partial"), error = TRUE)
 })
 
 test_that("can create and delete content", {
