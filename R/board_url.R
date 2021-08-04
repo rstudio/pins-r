b0e2e673c83b7993bb7c2f457e7719243c3ecf38
Hadley Wickham <h.wickham@gmail.com>
2021-08-04 13:47:23 -0500

    Explain how you might get data.txt

--- R/board_url.R
+++ R/board_url.R
@@ -12,7 +12,9 @@
 #' `board_url()` is read only and does not currently support versions.
 #'
 #' @param urls A named character vector of URLs If the URL ends in a `/`,
-#'   `board_url` will look for a `data.txt` that provides metadata.
+#'   `board_url` will look for a `data.txt` that provides metadata. The
+#'   easiest way to generate this file is to upload a pin directory created by
+#'   [board_folder()].
 #' @param use_cache_on_failure If the url fails to download, is it ok to
 #'   use the last cached failure? Defaults to `is_interactive()` so you'll
 #'   be robust to poor internet connectivity when exploring interactively,
