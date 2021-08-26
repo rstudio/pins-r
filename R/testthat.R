df46c601fae71d78414c29b55cf6c2581cc6edf3
Hadley Wickham <h.wickham@gmail.com>
2021-08-26 08:09:01 -0500

    Add missing namespace prefix

--- R/testthat.R
+++ R/testthat.R
@@ -81,7 +81,7 @@ test_api_basic <- function(board) {
 
   # Extra slash added for boards that allow pins in the form user/name
   testthat::test_that("can't use slashes in pin names", {
-    expect_error(
+    testthat::expect_error(
       pin_write(board, 1, "abc/def/ghijkl"),
       class = "pins_check_name"
     )
