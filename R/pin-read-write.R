46069d5e19c52f63066128b91703711a6d285308
Sean <smingerson@gmail.com>
2021-09-13 17:21:36 -0500

    Add qs format

--- R/pin-read-write.R
+++ R/pin-read-write.R
@@ -111,14 +111,15 @@ guess_type <- function(x) {
 }
 
 object_write <- function(x, path, type = "rds") {
-  type <- arg_match0(type, c("rds", "json", "arrow", "pickle", "csv"))
+  type <- arg_match0(type, c("rds", "json", "arrow", "pickle", "csv", "qs"))
 
   switch(type,
     rds = write_rds(x, path),
     json = jsonlite::write_json(x, path, auto_unbox = TRUE),
     arrow = arrow::write_feather(x, path),
     pickle = abort("'pickle' pins not supported in R"),
-    csv = utils::write.csv(x, path, row.names = FALSE)
+    csv = utils::write.csv(x, path, row.names = FALSE),
+    qs = write_qs(x, path)
   )
 
   path
@@ -143,7 +144,13 @@ write_rds <- function(x, path) {
   invisible(path)
 }
 
-object_types <- c("rds", "json", "arrow", "pickle", "csv", "file")
+write_qs <- function(x, path) {
+  check_installed("qs")
+  qs::qsave(x, path)
+  invisible(path)
+}
+
+object_types <- c("rds", "json", "arrow", "pickle", "csv", "qs", "file")
 
 object_read <- function(meta) {
   path <- fs::path(meta$local$dir, meta$file)
@@ -161,6 +168,7 @@ object_read <- function(meta) {
       arrow = arrow::read_feather(path),
       pickle = abort("'pickle' pins not supported in R"),
       csv = utils::read.csv(path, stringsAsFactors = TRUE),
+      qs = read_qs(path),
       file = abort(c(
         "Pin does not declare file type so can't be automatically read",
         i = "Retrieve uploaded paths with `pin_download()`"
@@ -177,6 +185,11 @@ object_read <- function(meta) {
       files = pin_load.files(path)
     )
   }
+}
+
+read_qs <- function(path) {
+  check_installed("qs")
+  qs::qread(path, strict = TRUE)
 }
 
 is_prefix <- function(prefix, string) {
