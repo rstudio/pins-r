84384c302a763dfd62f6690b92b05e33fa8dd682
Hadley Wickham <h.wickham@gmail.com>
2021-12-13 17:19:32 -0600

    Try setting USER_CACHE and USER_DATA to tempfiles

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library(testthat)
 library(pins)
 
+# When testing from R CMD check, don't write into standard config directories
+Sys.setenv(R_USER_CACHE_DIR = tempfile(), R_USER_DATA_DIR = tempfile())
+
 test_check("pins")
