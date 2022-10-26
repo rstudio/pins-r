c4dcef1df57111a1f1653c125cb997738d3945e5
Julia Silge <julia.silge@gmail.com>
2022-10-26 11:42:39 -0600

    Add test

---
+++ tests/testthat/test-board_manifest.R
@@ -0,0 +1,20 @@
+test_that("can write manifest", {
+  b <- board_temp()
+  pin_write(b, mtcars, "mtcars-csv", type = "csv")
+  pin_write(b, mtcars, "mtcars-json", type = "json")
+  board_manifest(b)
+
+  manifest <- yaml::read_yaml(fs::path(b$path, "pins.txt"))
+
+  # names are correct
+  expect_identical(names(manifest), pin_list(b))
+
+  # values (relative paths to versions) are correct
+  imap(
+    manifest,
+    ~expect_identical(
+      .x,
+      append_slash(fs::path(.y, pin_versions(b, .y)$version))
+    )
+  )
+})
