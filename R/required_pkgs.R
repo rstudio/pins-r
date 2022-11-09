9e2e18328167d44e93e32e563fa4a12fd6bc4c85
Julia Silge <julia.silge@gmail.com>
2022-11-09 13:04:26 -0700

    Add `required_pkgs()` for pin boards

---
+++ R/required_pkgs.R
@@ -0,0 +1,14 @@
+#' Determine required packages for a pins board
+#'
+#' @param x A pin board
+#' @param ... Not used.
+#' @return A character vector of package names required to use the board.
+#' @name required_pkgs.pins_board
+#' @examples
+#' required_pkgs(board_temp())
+#'
+#' @export
+required_pkgs.pins_board <- function(x, ...) {
+  ellipsis::check_dots_empty()
+  character(0)
+}
