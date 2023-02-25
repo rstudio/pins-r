1596e9235b9e5c679509a4964094dcf71ea83bb7
Julia Silge <julia.silge@gmail.com>
2023-02-24 18:40:36 -0700

    Import purrr::possibly

--- R/pins-package.R
+++ R/pins-package.R
@@ -4,7 +4,7 @@
 ## usethis namespace: start
 #' @import rlang
 #' @importFrom purrr map map_chr map_dbl map_lgl map_int
-#' @importFrom purrr imap compact map2_chr
+#' @importFrom purrr imap compact map2_chr possibly
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
