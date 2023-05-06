fe3d1f83872b720c0941eb2e72ea3fc474f79567
Julia Silge <julia.silge@gmail.com>
2023-05-06 12:48:00 -0600

    Make API version an integer

--- R/meta.R
+++ R/meta.R
@@ -17,7 +17,7 @@ read_meta <- function(path) {
     yaml$user <- yaml$user %||% list()
   } else if (yaml$api_version > 1) {
     abort(c(
-      paste0("Metadata requires pins ", yaml$api_version, ".0.0 or greater"),
+      paste0("Metadata requires pins ", yaml$api_version),
       i = "Do you need to upgrade the pins package?"
     ))
   }
@@ -46,7 +46,7 @@ standard_meta <- function(paths,
     description = description,
     tags = if (is.null(tags)) tags else as.list(tags),
     created = format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = "UTC"),
-    api_version = 1
+    api_version = 1L
   )
 }
 
