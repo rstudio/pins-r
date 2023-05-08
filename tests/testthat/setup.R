79efc74f6c32e0f276400fc8549c559804e49030
Julia Silge <julia.silge@gmail.com>
2023-05-08 09:45:04 -0600

    Switch to webfakes httpbin

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,2 +1,11 @@
 options(pins.verbose = FALSE)
 options(pins.quiet = TRUE)
+
+httpbin <- webfakes::local_app_process(
+  if (rlang::is_installed("webfakes")) webfakes::httpbin_app(),
+  .local_envir = testthat::teardown_env()
+)
+
+redact_port <- function(snapshot) {
+  snapshot <- gsub(httpbin$get_port(), "<port>", snapshot, fixed = TRUE)
+}
