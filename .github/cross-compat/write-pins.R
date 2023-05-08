fdccd27bf29ab6ac5c06ed4c56117147ae40bd33
Julia Silge <julia.silge@gmail.com>
2023-05-08 14:14:19 -0600

    Write with tags in R

--- .github/cross-compat/write-pins.R
+++ .github/cross-compat/write-pins.R
@@ -2,4 +2,4 @@ library(pins)
 args <- commandArgs(trailingOnly = TRUE)
 
 board <- board_folder(args[1])
-board %>% pin_write(mtcars, "mtcars-r", type = "csv")
+board %>% pin_write(mtcars, "mtcars-r", type = "csv", tags = c("cars", "fuel"))
