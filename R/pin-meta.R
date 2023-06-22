ec2fe415ea930cec678f983b2cccf5a55d111ba6
Julia Silge <julia.silge@gmail.com>
2023-06-22 15:32:15 -0600

    Fix implementation of `possibly_pin_meta`

--- R/pin-meta.R
+++ R/pin-meta.R
@@ -46,7 +46,9 @@ pin_meta <- function(board, name, version = NULL, ...) {
   UseMethod("pin_meta")
 }
 
-possibly_pin_meta <- possibly(pin_meta)
+possibly_pin_meta <- function(...) {
+  purrr::possibly(pin_meta)(...)
+}
 
 multi_meta <- function(board, names) {
   meta <- map(names, possibly(pin_meta, empty_local_meta), board = board)
