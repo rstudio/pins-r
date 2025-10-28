61ea289a97bba16c101a98bdfb7d898e6fc2f629
Neal Richardson <neal.p.richardson@gmail.com>
2025-10-28 12:46:37 -0400

    Move connect test helpers out of the package

--- R/board_connect.R
+++ R/board_connect.R
@@ -730,32 +730,3 @@ rsc_version <- function(board) {
 rsc_v1 <- function(...) {
   paste0(c("v1", ...), collapse = "/")
 }
-
-# Testing setup -----------------------------------------------------------
-
-board_connect_test <- function(...) {
-  if (nzchar(Sys.getenv("CONNECT_API_KEY"))) {
-    board_connect(auth = "envvar", ...)
-  } else if (connect_has_ptd()) {
-    board_connect(
-      ...,
-      server = "pub.demo.posit.team",
-      auth = "rsconnect",
-      cache = fs::file_temp()
-    )
-  } else {
-    testthat::skip(
-      "board_connect_test() requires CONNECT_API_KEY or Posit's demo PTD server"
-    )
-  }
-}
-
-# Use demo.posit.team PTD for local testing
-connect_has_ptd <- function() {
-  accounts <- rsconnect::accounts()
-  if (is.null(accounts) || nrow(accounts) == 0) {
-    FALSE
-  } else {
-    "pub.demo.posit.team" %in% accounts$server
-  }
-}
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,31 @@
 options(pins.verbose = FALSE)
 options(pins.quiet = TRUE)
 options(googledrive_quiet = TRUE)
+
+# Connect testing: prefers using CONNECT_SERVER and CONNECT_API_KEY env vars,
+# but can fall back to Posit's demo PTD server if you've logged in with rsconnect
+board_connect_test <- function(...) {
+  if (nzchar(Sys.getenv("CONNECT_API_KEY"))) {
+    board_connect(auth = "envvar", ...)
+  } else if (connect_has_ptd()) {
+    board_connect(
+      ...,
+      server = "pub.demo.posit.team",
+      auth = "rsconnect",
+      cache = fs::file_temp()
+    )
+  } else {
+    testthat::skip(
+      "board_connect_test() requires CONNECT_API_KEY or Posit's demo PTD server"
+    )
+  }
+}
+
+connect_has_ptd <- function() {
+  accounts <- rsconnect::accounts()
+  if (is.null(accounts) || nrow(accounts) == 0) {
+    FALSE
+  } else {
+    "pub.demo.posit.team" %in% accounts$server
+  }
+}
